f58480418090392adfc2ced98f515c2a56567af8
yugasun <yuga_sun@163.com>
2020-08-06 11:48:43 +0800

    fix: get post data bug

--- src/_shims/sl_handler.php
+++ src/_shims/sl_handler.php
@@ -54,12 +54,21 @@ function handler($event, $context)
     $_GET = $event->queryString ?? [];
     $_GET = json_decode(json_encode($_GET), true);
 
+    $_SERVER['REQUEST_METHOD'] = $event->httpMethod;
+
     if(!empty($event->body)) {
+      $jsonobj = json_decode($event->body, true);
+      if (is_array($jsonobj) && !empty($jsonobj)) {
+        foreach ($jsonobj as $k => $v) {
+          $_POST[$k] = $v;
+        }
+      } else {
         $_POSTbody = explode("&", $event->body);
         foreach ($_POSTbody as $postvalues) {
-            $tmp=explode("=", $postvalues);
-            $_POST[$tmp[0]] = $tmp[1];
+          $tmp=explode("=", $postvalues);
+          $_POST[$tmp[0]] = $tmp[1];
         }
+      }
     }
 
     $app = new \think\App();
