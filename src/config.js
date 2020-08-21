6154178c561ef1b068cf5215a0b2a472eb14b923
Peter Shen <zpshen@gmail.com>
2020-08-21 15:15:35 +0800

    merge from main respo

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const CONFIGS = {
+  templateUrl:
+    'https://serverless-templates-1300862921.cos.ap-beijing.myqcloud.com/thinkphp-demo.zip',
+  compName: 'thinkphp',
+  compFullname: 'thinkphp',
+  handler: 'sl_handler.handler',
+  runtime: 'Php7',
+  timeout: 3,
+  memorySize: 128,
+  namespace: 'default',
+  description: 'Created by Serverless Component'
+}
+
+module.exports = CONFIGS
